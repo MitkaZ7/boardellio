14335c62757c3396ebffd3d3094b781a0ea1b583
Дмитрий Западнов <mitkamac@macbook.local>
2023-10-11 16:01:38 +0300

    dev: add side menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/icons/logo.svg'
 import Navbar from '../Navbar/Navbar'
-import MenuToggler from '../MenuToggler/MenuToggler'
+// import MenuToggler from '../MenuToggler/MenuToggler'
 const Header = () => {
+ 
+
   return (
     <header className='header'>
-        <img src={logo} className='header__logo'></img>
+      <img src={logo} className='header__logo'></img>
       <Link to='/'>
         <h3 className='header__title'>My Dashboard</h3>
       </Link>
-      <MenuToggler/>
-        {/* <Navbar /> */}
+      
+      <Navbar />
 
     </header>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import MenuToggler from '../MenuToggler/MenuToggler'
 const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const activeMenuClassName = 'navbar_state_active';
+  function handleOpenMenu() {
+    setMenuOpen(!isMenuOpen);
+  }
   return (
-    <ul className='navbar'>
-      <li className='navbar__item'><Link to='/projects' className='navbar__link'>All projects</Link></li>
-      <li className='navbar__item'><Link to='tasks' className='navbar__link'>Project tasks</Link></li>
-      <li className='navbar__item'><Link to='tasks' className='navbar__link'>Contact me</Link></li>
-      <li className='navbar__item'><Link to='tasks' className='navbar__link'>My github</Link></li>
-    </ul>
+    <div className='menu'>
+      <ul className={`navbar ${isMenuOpen ? activeMenuClassName : ''}`}>
+        <li className='navbar__item'><Link to='/projects' className='navbar__link'>Projects list</Link></li>
+        <li className='navbar__item'><Link to='tasks' className='navbar__link'>Project tasks</Link></li>
+        <li className='navbar__item'><Link to='tasks' className='navbar__link'>Contact me</Link></li>
+        <li className='navbar__item'><Link to='tasks' className='navbar__link'>My github</Link></li>
+      </ul>
+      <MenuToggler onClick={handleOpenMenu} isMenuOpen={isMenuOpen}/>
+    </div>
   )
 }
 

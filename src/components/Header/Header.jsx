996cea194af262ebbdb7d3572c69ba53c61864dc
MitkaZ7 <mitka.dev@gmail.com>
2023-10-31 09:21:17 +0300

    dev: test commit via vsc panel

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import logo from '../../assets/icons/logo.svg'
 import Navbar from '../Navbar/Navbar'
 import FadeInAnimation from "../FadeInAnimtion/FadeInAnimation";
-// import MenuToggler from '../MenuToggler/MenuToggler'
+
 const Header = () => {
  
 

c130549d0131c1c092a3362e2493a01683fdcf3b
MitkaZ7 <mitka.dev@gmail.com>
2023-11-03 17:36:12 +0300

    dev: little refact of validation.js

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,28 +1,25 @@
 import Joi from 'joi'
-const formFieldsOption = () => {
-    return {
-        email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'ru'] } }).min(6).max(20),
-        password: Joi.string().min(6).max(30).required(),
-        confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
-            'any.only': 'Passwords do not match',
-            'any.required': 'Confirm password is required',
-        }),
-    }
-}
 
-export const loginSchema = Joi.object({
+const options = {
     email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'ru'] } }).min(6).max(20),
     password: Joi.string().min(6).max(30).required(),
+    confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
+        'any.only': 'Passwords do not match',
+        'any.required': 'Confirm password is required',
+    }),
+}
+
+
+export const loginSchema = Joi.object({
+    email: options.email,
+    password: options.password,
 
 });
 
 export const registrationSchema = Joi.object({
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'ru'] } }).min(6).max(20),
-    password: Joi.string().min(6).max(30).regex(/^[a-zA-Z0-9]{3,30}$/),
-    confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
-            'any.only': 'Passwords do not match',
-            'any.required': 'Confirm password is required',
-        }),
+    email: options.email,
+    password: options.password,
+    confirmPassword:  options.confirmPassword,
 });
 
 // export const schema = Joi.object({

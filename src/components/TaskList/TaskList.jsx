35c93f17f38ff44676f1065c8471997a9635ded4
MitkaZ7 <mitka.dev@gmail.com>
2023-12-21 16:13:24 +0300

    dev: force dnd work

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,42 +1,54 @@
-import React, { useEffect  } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useDrop } from 'react-dnd';
 import TaskCard from '../TaskCard/TaskCard';
-import { getTasks, updateTaskStatus, setSelectedTaskStatus, getOneTask } from '../../store/slices/tasksSlice';
+import { getTasks, updateTaskStatus } from '../../store/slices/tasksSlice';
 import Loader from '../Loader/Loader';
 import { openCustomPopup } from '../../store/slices/popupSlice';
-import TaskPopup from '../TaskPopup/TaskPopup';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { DndProvider, useDrop } from 'react-dnd';
+import { ItemTypes } from '../../utils/constants';
 
 const TaskList = ({ onClick, taskStatus }) => {
-  const { isLoading } = useSelector((state) => state.loader);
-  const { selectedTaskId } = useSelector(state => state.tasks)
+  const refs = useRef({});
+  const listRef = useRef(); 
   const dispatch = useDispatch();
-  const openTaskPopupHandler = () => {
-    // const popupData = { someKey: 'someValue' }; // Дополнительные данные, если нужны
-    openCustomPopup(dispatch, 'TaskPopup');
-    // dispatch(getOneTask(selectedTaskId))
-  };
-  
-  const handleTaskClick = () => {
-    openTaskPopupHandler(selectedTaskId);
-    onClick()
-   
+
+  const handleDrop = (taskId, newStatus) => {
+    console.log('item dropped:', taskId);
+    dispatch(updateTaskStatus({ taskId, newStatus }));
   };
 
+
+  const [{ isOver }, drop] = useDrop({
+    accept: ItemTypes.TASK_CARD,
+    drop: (item) => handleDrop(item.taskId, taskStatus),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  });
+
+  const { isLoading } = useSelector((state) => state.loader);
+  const { selectedTaskId } = useSelector((state) => state.tasks);
+
+  const openTaskPopupHandler = () => {
+    openCustomPopup(dispatch, 'TaskPopup');
+  };
+
+  const handleTaskClick = () => {
+    openTaskPopupHandler(selectedTaskId);
+    onClick();
+  };
 
   const { tasks } = useSelector((state) => state.tasks);
   const filteredTasks = tasks[taskStatus] || [];
+
   useEffect(() => {
     dispatch(getTasks());
-  }, []); 
-
-  
+  }, [dispatch]);
 
   return (
-    <DndProvider backend={HTML5Backend}>
+    <div ref={drop} className="task-list-container">
       {isLoading && <Loader />}
-      <ul className="taskList">
+      <ul className="taskList" >
         {filteredTasks.map((task) => (
           <TaskCard
             key={task.taskId}
@@ -45,11 +57,13 @@ const TaskList = ({ onClick, taskStatus }) => {
             status={taskStatus}
             taskId={task.taskId}
             onClick={handleTaskClick}
+            ref={(element) => {
+              refs[task.taskId] = element;
+            }}
           />
         ))}
-        {/* {activeTaskId && <TaskPopup taskId={activeTaskId} />} */}
       </ul>
-    </DndProvider>
+    </div>
   );
 };
 
--- src/components/hoc/RequireAuth.jsx
+++ src/components/hoc/RequireAuth.jsx
@@ -2,6 +2,7 @@ import { useLocation, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import React from 'react'
 const RequireAuth = ({children}) => {
+  const userData = localStorage.getItem('userData');
     const location = useLocation();
     const {isAuthorized} = useSelector(state => state.user);
     if (!isAuthorized) {

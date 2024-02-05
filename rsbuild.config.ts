559d8e5540684fbc56f2f35fc1ce311baa64a3fe
MitkaZ7 <mitka.dev@gmail.com>
2024-02-05 17:24:57 +0300

    dev: migrate to rsbuild

---
+++ rsbuild.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig, loadEnv } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+const { publicVars } = loadEnv({ prefixes: ['REACT_APP_'] });
+export default defineConfig({
+    plugins: [pluginReact()],
+    output: {
+        distPath: {
+            root: 'dist',
+        },
+    },
+    source: {
+        define: publicVars,
+    },
+});

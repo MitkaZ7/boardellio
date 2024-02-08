40f704dacdc083cf29ae6d67ba20a492cb693c19
MitkaZ7 <mitka.dev@gmail.com>
2024-02-08 22:57:37 +0300

    fix: remove comment

--- src/store/middlewares/localStorageMiddleware.js
+++ src/store/middlewares/localStorageMiddleware.js
@@ -1,6 +1,5 @@
 import { setUser, authorizeUser, setAuthorizationStatus } from "../slices/userSlice";
 const localStorageMiddleware = (store) => (next) => (action) => {
-    // вызов middleware после того, как обновится состояние
     const result = next(action);
     if (action.type === setUser.type || action.type === setAuthorizationStatus.type) {
         const { user } = store.getState();

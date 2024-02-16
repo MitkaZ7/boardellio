b66baaffde6470fcdcfed37e0a9d679790bf73e5
MitkaZ7 <mitka.dev@gmail.com>
2024-02-16 16:36:14 +0300

    dev: add changelog component

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+This project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+
+## [1.0.0] - 2024-02-15
+### Added
+- New feature A
+- New feature B
+
+### Changed
+- Updated dependency X to version Y
+
+### Fixed
+- Issue with component Z
+
+[Unreleased]: https://github.com/username/repo/compare/v1.0.0...HEAD
+[1.0.0]: https://github.com/username/repo/releases/tag/v1.0.0
--- src/components/ChangeLog/Changelog.jsx
+++ src/components/ChangeLog/Changelog.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import packageJson from '../../../package.json';
+import ReactMarkdown from 'react-markdown';
 const Changelog = () => {
-    
-  const version = packageJson.version; 
+    const [changelog, setChangelog] = useState('');      
+    const version = packageJson.version; 
 
+    useEffect(() => {
+        async function fetchChangeLog() {
+            const response = await fetch('CHANGELOG.md');
+            const text = await response.text();
+            setChangelog(text);
+        }
+        fetchChangeLog();
+    }, []);
   return (
+    <>  
       <div>Changelog {version}</div>
+          <ReactMarkdown>{changelog}</ReactMarkdown>
+    </>
+      
+      
     
   )
 }

38222e7959fb33358a835a1105fc7dc81530417b
MitkaZ7 <mitka.dev@gmail.com>
2024-02-19 16:41:47 +0300

    dev: migrate to firestore start

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-const UserProfile = ({user}) => {
+const avatar = require('../../assets/images/avatar.png');
+import { useDispatch, useSelector } from 'react-redux';
+
+const UserProfile = () => {
     const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const { user } = useSelector((state)=> state.user);
+    console.log(user)
   return (
-    <div>UserProfile</div>
+    <div className='user-profile'>
+      <div className="user-profile__header">
+        <img src={avatar} alt="user avatar" className="user-profile__avatar" />
+      </div>
+      <div className="user-profile__info">
+        <div className="user-profile__info-item">
+          email: 
+          <span> {user.email}</span> 
+        </div>
+        <div className="user-profile__info-item">
+          name:
+          <span> Jhon Travolta</span>
+        </div>
+        <div className="user-profile__info-item">
+          role:
+          <span> user</span>
+        </div>
+      </div>
+      <div className="user-profile__projects">
+        <div className="">
+          <h3 ser-profile__projects-header>
+            My projects:
+          </h3>
+        </div>
+        <div className="">
+          <h3 ser-profile__projects-header>
+            My tasks:
+          </h3>
+        </div>
+      </div>
+
+    </div>
   )
 }
 

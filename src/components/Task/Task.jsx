f467e15183b9546764d38e34cef7ed7b4434bfad
MitkaZ7 <mitka.dev@gmail.com>
2024-02-28 00:13:51 +0300

    dev: переделать стили у компонета задачи.

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -3,17 +3,27 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getOneTask, logicDeleteTask, resetSelectedTaskData,getTasks } from '../../store/slices/tasksSlice';
 import Upload from '../../assets/icons/upload.svg';
 import { closePopup } from '../../store/slices/popupSlice';
+import { useNavigate } from 'react-router-dom';
+
 import Tooltip from '../Tooltip/Tooltip'
 import { showLoader, hideLoader } from '../../store/slices/loaderSlice';
 import { daysCount, formateDate } from '../../utils/formateDate'
 import { useTranslation } from 'react-i18next'
 const Task = ({ taskId }) => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
   const dispatch = useDispatch();
   const { selectedTaskData, selectedTaskId } = useSelector(state => state.tasks);
-  const { projectName } = useSelector(state => state.projects.selectedProject);
+  const projecTitle  = useSelector(state => state.projects.selectedProject.title.stringValue);
   const projectTag = useSelector(state => state.projects.selectedProject.tag.stringValue);
-  const [taskWorkTime, setTaskWorkTime] = useState('')
+  const [taskWorkTime, setTaskWorkTime] = useState('');
+
+
+  const navigateToProjectPage = () => {
+    dispatch(closePopup());
+  }
+
   const handleRemoveTask = () => {
     dispatch(logicDeleteTask(selectedTaskId))
       .then(() => dispatch(closePopup()))
@@ -43,31 +53,34 @@ const Task = ({ taskId }) => {
     <li className='task'>
       <article className='task__content'>
         <header className='task__header'>
-          
           <h3 className='task__task-title'>
-                <span className='task__metadata-item task__number'>{projectTag}-{selectedTaskData.number.integerValue} </span>
-            {selectedTaskData.title.stringValue}
+                <span className='task__data-item task__number' onClick={navigateToProjectPage}>
+              {projectTag}-{selectedTaskData.number.integerValue}: 
+            </span>
+                &nbsp;{selectedTaskData.title.stringValue}
           </h3>
-          <h4 className='task__project-title'>
-            {projectName}
-          </h4>
+          <p className='task__data-item task__status'>
+            {t('status')}:&nbsp;{selectedTaskData.status.stringValue}
+          </p>
           
-              
-          <div className='task__metadata-parametres'>
-                <span className='task__metadata-item task__priority'>proirity: {selectedTaskData.priority.stringValue}</span>
-
-                <span className='task__metadata-item task__status'>status: {selectedTaskData.status.stringValue}</span>
-          </div>
+              <h4 className='task__project-title' onClick={navigateToProjectPage}>
+                {projecTitle}
+          </h4>
         </header>
-        <section className='task__metadata-block metadata-block'>
-          <div className='task__metadata-dates'>
-                <span className='task__metadata-item task__creation-date'>{t('created')}: {formateDate(selectedTaskData.createTime)}&nbsp;</span>
-            {/* <span className='task__metadata-item task__spent-time'>In work for: {
+        <section className='task__data-block data-block'>
+          <div className='task__data-dates'>
+                <span className='task__data-item task__creation-date'>{t('created')}:&nbsp;  
+              {formateDate(selectedTaskData.createTime)}
+            </span>
+                <span className='task__metadata-item task__work-time'>{t('in-work')}: 3 дня {
                   daysCount(selectedTaskData.createTime)
             }
-            </span> */}
-
-            {
+            </span>
+          </div>
+              <p className='task__data-item task__priority'>
+                {t('priority')}: {selectedTaskData.priority.stringValue}
+              </p>
+            {/* {
                 selectedTaskData.isCompleted.booleanValue ? 
                     <span className='task__metadata-item task__finish-date'>
                       &nbsp;{t('done')}: сегодня
@@ -76,12 +89,12 @@ const Task = ({ taskId }) => {
                     <span className='task__metadata-item task__finish-date'>
                       &nbsp;{t('done')}: не завершена.
                     </span>
-            }
+            } */}
 
             {/* <span className='task__metadata-item task__finish-date'>&nbsp;{t('done')}: {
                   !selectedTaskData.isCompleted.booleanValue ? 'DDD' : 'NNNN'
             }</span> */}
-          </div>
+          
           <div className="task__controls">
             {/* <button className="task__controls-btn task-button task__controls-btn_type_edit"></button> */}
             <button className="task__controls-btn task-button task__controls-btn_type_delete" onClick={handleRemoveTask}></button>

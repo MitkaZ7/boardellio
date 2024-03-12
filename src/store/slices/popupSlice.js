77c41e50091f51ab8c5fd7fb983ba4a7292cd49d
MitkaZ7 <mitka.dev@gmail.com>
2024-03-13 00:53:11 +0300

    dev: remove unuseable reducers

--- src/store/slices/popupSlice.js
+++ src/store/slices/popupSlice.js
@@ -4,35 +4,16 @@ const initialState = {
     isOpen: false,
     popups: [],
     openedPopups: {},
-    // taskPopupIsOpen: false,
     openedTaskId: null,
-    // addTaskPopupIsOpen: false,
-    activePopup: '',
-    addProjectPopup: {
-        isOpen: false,
-    },
-    confrimPopup : {
-        isOpen: false,
-    }
+  
 }
 
 
-export const openCustomPopup = (dispatch, name, data = null) => {
-    if (name === 'TaskPopup') {
-        // console.log(name + ' is open ')
-    }
-    dispatch(openPopup({ name, data }));
-};
+
 const popupSlice = createSlice({
     name: 'popup',
     initialState,
     reducers: {
-        // openConfirmPopup: (state,action) => {
-        //     state.confrimPopup.isOpen = true
-        // },
-        // closeConfirmPopup: (state, action) => {
-        //     state.confrimPopup.isOpen = false
-        // },
         openPopup: (state, action) => {
             const { name } = action.payload;
             state.openedPopups[name] = { isOpen: true };
@@ -41,31 +22,9 @@ const popupSlice = createSlice({
             const { name } = action.payload;
             delete state.openedPopups[name]; 
         },
-        openPopupOld: (state, action) => {
-            state.popups = [];
-            state.isOpen = true;
-            state.activePopup = action.payload.name;
-            state.popupData = action.payload.data; 
-        },
-        closePopupOld: (state, action) => {
-            state.isOpen = false;
-            state.activePopup = ''; 
-            state.popupData = null; 
-            
-        },
-        openTaskPopup: (state, action) => {
-            state.isOpen = true;
-            state.activePopup = action.payload.name;
-            state.openedTaskId = action.payload; 
-        },
-
-        closeTaskPopup: (state, action) => {
-            state.taskPopupIsOpen = false;
-            state.openedTaskId = null; 
-        },
     },
   
 });
 
-export const { openPopup, closePopup, openTaskPopup, closeTaskPopup } = popupSlice.actions;
+export const { openPopup, closePopup } = popupSlice.actions;
 export default popupSlice.reducer;

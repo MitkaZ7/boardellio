4a8c1f72e7e164170b699a3e2b159117d352a414
MitkaZ7 <mitka.dev@gmail.com>
2024-03-27 13:46:35 +0300

    dev: hide log

--- src/store/middlewares/rejectionMiddleware.js
+++ src/store/middlewares/rejectionMiddleware.js
@@ -1,7 +1,7 @@
 
 const rejectionMiddleware = () => (next) => (action) => {
     if (action.type.endsWith('/rejected')) {
-        console.log('Async action failed with error:', action.error);
+        // console.log('Async action failed with error:', action.error);
     }
     return next(action);
 };
